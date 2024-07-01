58336e8902b06b13cc071a098b90da2b7bfe09aa
rauudy <raudyc@gmail.com>
2024-06-30 23:23:33 -0600

    Front1

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { RegistroComponent } from './components/auth/registro/registro.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 export const routes: Routes = [
     {
-        path: 'login', 
-        component: RegistroComponent
+        path: '', 
+        component: LoginComponent
     },
     {
-        path: '',
+        path: 'registrar',
         component: RegistroComponent
     }
 ];
--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterOutlet, RouterModule } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UsuarioService } from '../../../services/usuario.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -8,5 +14,8 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  constructor(
+    private http: UsuarioService,
+    private router: Router
+  ){}
 }
