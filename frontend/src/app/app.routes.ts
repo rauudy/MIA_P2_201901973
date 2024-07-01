6f9ff52e5c4a4b8e5c71c78e1ffe9ae7016066d5
rauudy <raudyc@gmail.com>
2024-06-30 23:43:18 -0600

    Front3

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegistroComponent } from './components/auth/registro/registro.component';
-import { LoginComponent } from './components/auth/login/login.component';
 
 export const routes: Routes = [
     {
         path: 'login', 
-        component: LoginComponent
+        component: RegistroComponent
     },
     {
         path: '',
